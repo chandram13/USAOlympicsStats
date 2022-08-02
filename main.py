952dd031821810978965eb7e34ebbc8a507bff03
Marvish <mschandr@uci.edu>
2022-08-02 09:22:51 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,132 @@
+#   Marvish Chandra
+
+class Gender1Statistics:
+
+def __init__(self,country,totalTime,fisPoints):
+    self.country = country
+    self.totalTime = totalTime
+    self.fisPoints = fisPoints
+
+
+
+# top 3 US candidates
+# Results from Proctor Ski Area (1st Game in WO)
+
+def winterAlpine(self,name):
+    "0":{
+        "id": 106542394,
+        "year": 1999,
+        country: USA,
+        "events": [
+            {
+                "id": 1983,
+                "name": Brayton Hunter,
+                "totalTime": 90.63,
+                fisPoints: 41.77
+            }
+        ]
+    }
+    "1":{
+        "id": 306533028,
+        "year": 2002,
+        country: USA,
+        "events": [
+            {
+                "id": 2002,
+                "name": Kearing Luke,
+                "totalTime": 90.96,
+                fisPoints: 44.80
+
+            }
+        ]
+    }
+    "2":{
+        "id": 176532584,
+        "year": 2000,
+        country: USA,
+        "events": [
+            {
+                "id": 176532584,
+                "name": Underhill Bradshaw,
+                "totalTime": 91.02,
+                fisPoints: 45.28
+            }
+        ]
+    }
+
+# Park City, UT
+
+def winter_Ski_Jumping(self,name):
+    "0":{
+      "id": 75835,
+        "year": 1984,
+        "country": USA,
+        "events": [
+            "id": 4592,
+            "name": Welch Brian,
+            "totalDistance": 313.2
+            fisPoints: 755.1
+        ]
+    }
+    "1":{
+        "id": 75835,
+        "year": 1983,
+        country: USA,
+        "events": [
+            "id": 3013,
+            "name": Schwall Tommy,
+            "totalDistance": 369.6,
+            fisPoints: 755.1
+        ]
+    }
+    "2":{
+        "id": 75835,
+        "year": 1984,
+        country: USA,
+        "events": [
+            "id": 3157,
+            "name": Jones Clint,
+            "totalDistance": 110.925,
+            fisPoints: 755.1
+        ]
+    }
+    "3":{
+        "id": 75835,
+        "year": 1980,
+        country: USA,
+        "events": [
+            "id": 2077,
+            "name": Alborn Alan Jacob,
+            "totalDistance": 117.475
+            fisPoints: 755.1
+        ]
+
+
+# Salt Lake City
+def winterRunning(self,name):
+    "0":{
+        "id": 2855,
+        country: USA,
+        "events": [
+            {
+                "id": 1016754,
+                "name": Bauer John,
+                "totalTime": 152.506,
+                fisPoints: 64.06
+            }
+        ]
+    }
+    "1":{
+        "id": 2855,
+        country: USA,
+        "events": [
+            {
+                "id": 1285444
+                "name": Johnson Andrew
+                "totalTime": 152.443
+                fisPoints: 125.32
+            }
+        ]
+    }
+
+    }
---
+++ secondfile.py
@@ -0,0 +1,52 @@
+
+class Gender2Statistics:
+
+    def fwinterRunning(self):
+        "0": {
+            "id": 2856,
+            "year": 1970,
+            country: USA,
+            "events": [
+                {
+                    "id": 1004338,
+                    "name": Kemppell Nina,
+                    "totalTime": 114.14,
+                    fisPoints: 53.81
+                }
+            ]
+        }
+        "1": {
+            "id": 2856,
+            "year": 1973,
+            country: USA,
+            "events": [
+                {
+                    "id": 1155658,
+                    "name": Wagner Wendy Kay,
+                    "totalTime": 108.125
+                }
+            ]
+        }
+        "2": {
+            "id": 2856,
+            "year": 1977,
+            country: USA,
+            "events": [
+                {
+                    "id": 1196107,
+                    "name": Jones Barbara,
+                    "totalTime": 119.403
+                }
+            ]
+        }
+
+
+
+    def fwinterSwimming(self):
+
+
+
+
+
+
+    def fwinterSnowboarding(self):
