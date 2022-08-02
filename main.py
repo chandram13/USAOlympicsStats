f9cc0e75579c0a5c08372d3e560d85a488f043a1
Marvish <mschandr@uci.edu>
2022-08-02 10:41:24 -0700

    Updated second file with newest information found. Fixed simple syntax errors.

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ def __init__(self,country,totalTime,fisPoints):
 
 def winterAlpine(self,name):
     "0":{
-        "id": 106542394,
+        "codex": 106542394,
         "year": 1999,
-        country: USA,
-        "events": [
+        country: "USA",
+        "athleteProfile": [
             {
-                "id": 1983,
-                "name": Brayton Hunter,
+                "FIS": 1983,
+                "name": "Brayton Hunter",
                 "totalTime": 90.63,
                 fisPoints: 41.77
             }
         ]
     }
     "1":{
-        "id": 306533028,
+        "codex": 306533028,
         "year": 2002,
-        country: USA,
-        "events": [
+        country: "USA",
+        "athleteProfile": [
             {
-                "id": 2002,
-                "name": Kearing Luke,
+                "FIS": 2002,
+                "name": "Kearing Luke",
                 "totalTime": 90.96,
                 fisPoints: 44.80
 
@@ -41,13 +41,13 @@ def winterAlpine(self,name):
         ]
     }
     "2":{
-        "id": 176532584,
+        "codex": 176532584,
         "year": 2000,
-        country: USA,
-        "events": [
+        country: "USA",
+        "athleteProfile": [
             {
-                "id": 176532584,
-                "name": Underhill Bradshaw,
+                "FIS": 176532584,
+                "name": "Underhill Bradshaw",
                 "totalTime": 91.02,
                 fisPoints: 45.28
             }
@@ -58,45 +58,45 @@ def winterAlpine(self,name):
 
 def winter_Ski_Jumping(self,name):
     "0":{
-      "id": 75835,
+      "codex": 75835,
         "year": 1984,
-        "country": USA,
-        "events": [
-            "id": 4592,
-            "name": Welch Brian,
+        "country": "USA",
+        "athleteProfile": [
+            "FIS": 4592,
+            "name": "Welch Brian",
             "totalDistance": 313.2
             fisPoints: 755.1
         ]
     }
     "1":{
-        "id": 75835,
+        "codex": 75835,
         "year": 1983,
-        country: USA,
-        "events": [
-            "id": 3013,
-            "name": Schwall Tommy,
+        country: "USA",
+        "athleteProfile": [
+            "FIS": 3013,
+            "name": "Schwall Tommy",
             "totalDistance": 369.6,
             fisPoints: 755.1
         ]
     }
     "2":{
-        "id": 75835,
+        "codex": 75835,
         "year": 1984,
-        country: USA,
-        "events": [
-            "id": 3157,
-            "name": Jones Clint,
+        country: "USA",
+        "athleteProfile": [
+            "FIS": 3157,
+            "name": "Jones Clint",
             "totalDistance": 110.925,
             fisPoints: 755.1
         ]
     }
     "3":{
-        "id": 75835,
+        "codex": 75835,
         "year": 1980,
-        country: USA,
-        "events": [
-            "id": 2077,
-            "name": Alborn Alan Jacob,
+        country: "USA",
+        "athleteProfile": [
+            "FIS": 2077,
+            "name": "Alborn Alan Jacob",
             "totalDistance": 117.475
             fisPoints: 755.1
         ]
@@ -105,24 +105,24 @@ def winter_Ski_Jumping(self,name):
 # Salt Lake City
 def winterRunning(self,name):
     "0":{
-        "id": 2855,
-        country: USA,
-        "events": [
+        "codex": 2855,
+        country: "USA",
+        "athleteProfile": [
             {
-                "id": 1016754,
-                "name": Bauer John,
+                "FIS": 1016754,
+                "name": "Bauer John",
                 "totalTime": 152.506,
                 fisPoints: 64.06
             }
         ]
     }
     "1":{
-        "id": 2855,
-        country: USA,
-        "events": [
+        "codex": 2855,
+        country: "USA",
+        "athleteProfile": [
             {
-                "id": 1285444
-                "name": Johnson Andrew
+                "FIS": 1285444,
+                "name": "Johnson Andrew"
                 "totalTime": 152.443
                 fisPoints: 125.32
             }
--- secondfile.py
+++ secondfile.py
@@ -1,52 +1,127 @@
 
 class Gender2Statistics:
 
+
+# Snowbasin, UT
+def fwinterAlpine(self,name):
+    "0": {
+        "codex": 6414,
+        "year": 2002,
+        country: "USA",
+        "athleteProfile": [
+            {
+                "FIS": 536138,
+                "name": "Clark Kirsten L"
+                "totalTime": 76.67,
+                fisPoints: 21.55
+            }
+        ]
+    }
+    "1": {
+        "codex": 6414,
+        "year": 2002,
+        country: "USA",
+        "athleteProfile": [
+            {
+                "FIS": 536477,
+                "name": "Mendes Jonna"
+                "totalTime": 76.91,
+                fisPoints: 23.23
+            }
+        ]
+    }
+    "2": {
+        "codex": 6414,
+        "year": 2002,
+        country: "USA",
+        "athleteProfile": [
+            {
+                "FIS": 535529,
+                "name": "Monahan Kathleen"
+                "totalTime": 77.59
+                fisPoints: 27.99
+            }
+        ]
+    }
+# Pyenoungchang
+def fskiJumping(self,name):
+    "0": {
+        "codex": 3097,
+        "year": 2018,
+        country: "South Korea",
+        "athleteProfile": [
+            {
+                "FIS": 5399,
+                "name": "Hendrickson Sarah",
+                "totalDistance": 87
+                fisPoints: 160.6
+            }
+        ]
+    }
+    "1": {
+        "codex": 3097,
+        "year": 2018,
+        country: "South Korea",
+        "athleteProfile": [
+            {
+                "FIS": 2682,
+                "name": "Ringquist Abby",
+                "totalDistance": 84.25
+                fisPoints: 144.4
+            }
+        ]
+    }
+"2": {
+    "codex": 3097,
+    "year": 2018,
+    country: "South Korea",
+    "athleteProfile": [
+        {
+            "FIS": 6089,
+            "name": "Engulnd Nita"
+            "totalDistance": 77
+            fisPoints: 57.9
+        }
+    ]
+}
+
+#
     def fwinterRunning(self):
         "0": {
-            "id": 2856,
+            "codex": 2856,
             "year": 1970,
-            country: USA,
-            "events": [
+            country: "USA",
+            "athleteProfile": [
                 {
-                    "id": 1004338,
-                    "name": Kemppell Nina,
+                    "FIS": 1004338,
+                    "name": "Kemppell Nina",
                     "totalTime": 114.14,
                     fisPoints: 53.81
                 }
             ]
         }
         "1": {
-            "id": 2856,
+            "codex": 2856,
             "year": 1973,
-            country: USA,
-            "events": [
+            country: "USA",
+            "athleteProfile": [
                 {
-                    "id": 1155658,
-                    "name": Wagner Wendy Kay,
+                    "FIS": 1155658,
+                    "name": "Wagner Wendy Kay",
                     "totalTime": 108.125
                 }
             ]
         }
         "2": {
-            "id": 2856,
+            "codex": 2856,
             "year": 1977,
-            country: USA,
-            "events": [
+            country: "USA",
+            "athleteProfile": [
                 {
-                    "id": 1196107,
-                    "name": Jones Barbara,
+                    "FIS": 1196107,
+                    "name": "Jones Barbara",
                     "totalTime": 119.403
                 }
             ]
         }
 
-
-
-    def fwinterSwimming(self):
-
-
-
-
-
-
-    def fwinterSnowboarding(self):
